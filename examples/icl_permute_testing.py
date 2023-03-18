016770ed0f44d3f72a53eed9f372054cbdf518e4
Dhruv Pai <46631243+dhruvbpai@users.noreply.github.com>
2023-03-18 13:40:50 -0700

    Update icl_permute_testing.py

--- examples/icl_permute_testing.py
+++ examples/icl_permute_testing.py
@@ -96,15 +96,14 @@ for j in tqdm(range(args.iters)):
         input_ids = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0).to(device=device)
         with torch.inference_mode():
           logits = model(input_ids=input_ids).logits[:,-1].squeeze()
-        logits = normalize_probs(np.array([logits[word_maps.index(label)] for label in unique_labels]))
+        logits = normalize_probs(np.array([logits[word_maps.index(label)].cpu() for label in unique_labels]))
         ces[permutation] = cross_entropy_loss(logits[1],label_val)
-
         del logits
     ce_mean[j] = np.mean(ces)
-    ce_stdev[j] = np.stdev(ces)
+    ce_stdev[j] = np.std(ces)
 
     # Free up space
     del sample
 
 # Post processing output
-np.savetxt(args.out_path+"permute_%s_%s_%s.csv"%(args.size, args.iters, args.permute_count, np.stack([ce_mean, ce_stdev]).T, delimiter=",", header=["Mean CrossEntropy, Stdev CrossEntropy"]))
+np.savetxt(args.out_path+"permute_%s_%s_%s.csv"%(args.size, args.iters, args.permute_count), np.stack([ce_mean, ce_stdev]).T, delimiter=",", header=",".join(["Mean CrossEntropy", "Stdev CrossEntropy"]))
