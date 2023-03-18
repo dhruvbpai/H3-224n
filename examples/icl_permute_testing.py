76f9c4bff63cc91c89eb64dac11331210515938f
Dhruv Pai <46631243+dhruvbpai@users.noreply.github.com>
2023-03-18 12:13:19 -0700

    Permutation testing

--- examples/icl_permute_testing.py
+++ examples/icl_permute_testing.py
@@ -24,6 +24,7 @@ parser.add_argument('--separator', type=str, default=" is ", help="ICL separator
 parser.add_argument('--query', type=str, default="What is <QUERY>?")
 parser.add_argument('--size', type=int, default = 3, help="Size of ICL shot learning")
 parser.add_argument('--iters', type=int, default = 1, help ="Number of times to run ICL")
+parser.add_argument('--permute_count', type=int, default = 1, help ="Number of permutations to run")
 parser.add_argument('--out_path', type=str, help ="Where to save ICL dataset")
 args = parser.parse_args()
 
@@ -57,15 +58,21 @@ for name, module in model.named_modules():
     if isinstance(module, (torch.nn.Linear, torch.nn.Embedding, torch.nn.LayerNorm)):
         module.to(dtype=dtype)
 
-
+def cross_entropy_loss(yHat, y):
+    if y == 1:
+      return -np.log(yHat)
+    else:
+      return -np.log(1 - yHat)
+def normalize_probs(probs):
+    return probs * (1/np.sum(probs))
 # Load data
 with open(args.examples, "r") as corp, open(args.labels) as targ:
     data = np.array(corp.read().split("\n"))[:-1] # Change this based on how annoying your files are
     labels = np.array(targ.read().split("\n"))[:-1]
 
 unique_labels=np.unique(labels)
-predictions = np.zeros(args.iters)
-targs = np.zeros(args.iters)
+ce_mean = np.zeros(args.iters)
+ce_stdev = np.zeros(args.iters)
 word_maps = tokenizer.batch_decode([i for i in range(50000)])
 samples = np.stack((data, labels)).T
 for j in tqdm(range(args.iters)):
@@ -73,24 +80,31 @@ for j in tqdm(range(args.iters)):
     np.random.shuffle(samples)
     sample = samples[:args.size]
     targ_x, targ_y = sample[-1]
-    input_x = sample[:args.size, 0]
-    input_y = sample[:args.size, 1]
+    label_val = np.where(unique_labels == targ_y)[0][0]
+    sample=sample[:-1]
+    ces=np.zeros(args.permute_count)
+    for permutation in range(args.permute_count):
+        np.random.shuffle(sample)
+        input_x = sample[:, 0]
+        input_y = sample[:, 1]
 
-    # ICL Prompting
-    prompt = '\n'.join(["%s %s %s" % (input_x[i], args.separator,input_y[i]) for i in range(len(input_x))])+"\n"
-    prompt += args.query.replace("<QUERY>", targ_x)
+        # ICL Prompting
+        prompt = '\n'.join(["%s %s %s" % (input_x[i], args.separator,input_y[i]) for i in range(len(input_x))])+"\n"
+        prompt += args.query.replace("<QUERY>", targ_x)
 
-    # Perform Inference
-    input_ids = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0).to(device=device)
-    with torch.inference_mode():
-      logits = model(input_ids=input_ids).logits[:,-1].squeeze()
-    preds = [logits[word_maps.index(label)].cpu() for label in unique_labels]
-    predictions[j] = np.argmax(preds)
-    targs[j] = np.where(unique_labels == targ_y)[0][0]
+        # Perform Inference
+        input_ids = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0).to(device=device)
+        with torch.inference_mode():
+          logits = model(input_ids=input_ids).logits[:,-1].squeeze()
+        logits = normalize_probs(np.array([logits[word_maps.index(label)] for label in unique_labels]))
+        ces[permutation] = cross_entropy_loss(logits[1],label_val)
+
+        del logits
+    ce_mean[j] = np.mean(ces)
+    ce_stdev[j] = np.stdev(ces)
 
     # Free up space
-    del logits
     del sample
 
 # Post processing output
-np.savetxt(args.out_path+"out_%s_%s.csv"%(args.size, args.iters), np.stack([predictions, targs]).T, delimiter=",", header=",".join(unique_labels))
+np.savetxt(args.out_path+"permute_%s_%s_%s.csv"%(args.size, args.iters, args.permute_count, np.stack([ce_mean, ce_stdev]).T, delimiter=",", header=["Mean CrossEntropy, Stdev CrossEntropy"]))
